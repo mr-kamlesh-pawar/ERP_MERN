3f691721dcf734f6df87b93b6d16f52a76ae86e7
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2025-02-13 02:43:42 +0530

    Updated Student Dashboard

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -6,10 +6,10 @@ const AuthLayout = () => {
   
   return (
     <div className='flex min-h-screen w-full'>
-        <div className='hidden lg:flex items-center justify-center  w-1/2 px-12 bg-slate-100'>
+        <div className='hidden lg:flex items-center justify-center  w-1/2 px-12 bg-blue-100'>
         <div className='max-w-md space-y-6 text-center text-black text-primary-foreground'>
           
-          <img  className="" 
+          <img  className="rounded-lg" 
           src={clg} alt="" />
             <h1 className='text-xl font-extrabold tracking-tight text-black'>Welcome to Rasiklal M. Dhariwal Institute of Technology, Chinchwad</h1>
 
--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -5,6 +5,8 @@ import { loginUser } from '@/store/auth-slice';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 const AuthLogin = () => {
  const initialState = {
@@ -49,10 +51,14 @@ const AuthLogin = () => {
 
   return (
     <div className=' mx-auto w-full max-w-md space-y-6'>
-      <div className='text-center'>
+      <div className='text-center flex flex-col justify-center items-center'>
+      <img
+      src='http://rmdiot.in/images/estdImg.jpg'
+      />
+
         <h1 className='text-3xl font-bold tracking-tight text-foreground'>SignIn You Account</h1>
-        <p className='mt-2'>Don't have an account</p>
-        <Link to='/auth/register'  className='font-medium  text-primary hover:underline ml-2'>Create Account</Link>
+        <p className='mt-2'>Rasiklal M. Dhariwal Institute of Technology, Chinchwad</p>
+        <Link to='/'  className='font-medium  text-primary hover:underline ml-2'>🚀College ERP 🔑  </Link>
 
       </div>
       
@@ -65,6 +71,11 @@ const AuthLogin = () => {
         onSubmit={onSubmit}
 
       />
+<div className='w-[95%] flex justify-end  text-xs text-blue-800 font-bold'>
+<Link to="/">
+      <h1 className=''>Forgot Password ?</h1>
+</Link>
+</div>
     </div>
   )
 }
